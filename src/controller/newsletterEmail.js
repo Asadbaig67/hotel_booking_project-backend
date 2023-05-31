ee19cf18553914f69e0f161ff7c11238d9cc0241
Asadulhaq <2020cs667@student.uet.edu.pk>
2023-05-31 19:55:04 +0500

    News Letter Backend Added

--- src/controller/newsletterEmail.js
+++ src/controller/newsletterEmail.js
@@ -1,22 +1,36 @@
 import Newsletteremail from "../models/newsletterEmail.js";
+import { newsletterEmail } from "./mailer.js"
 
 export const addEmail = async (req, res) => {
-    const { email } = req.body;
-    try {
-        const newEmail = new Newsletteremail({
+   const { email } = req.body;
+
+   try {
+
+      const existingemail = await Newsletteremail.findOne({ email });
+      if (!existingemail) {
+         const newEmail = new Newsletteremail({
             email,
-        });
-        await newEmail.save();
-        res.status(201).json(newEmail);
-    } catch (error) {
-        res.status(409).json({ message: error.message });
-    }
+         });
+         const result = await newsletterEmail(email);
+         if (!result.status) {
+            return res.status(409).json({ message: result.message });
+         }
+         await newEmail.save();
+         return res.status(201).json({ message: "Email added successfully" });
+      }
+
+      return res.status(200).json({ message: "Email already exists" });
+
+   } catch (error) {
+      console.log(error);
+      // res.status(409).json({ message: error.message });
+   }
 };
 export const getAllEmails = async (req, res) => {
-    try {
-        const result = await Newsletteremail.find();
-        res.send(result);
-    } catch (error) {
-        res.status(404).json({ message: error.message });
-    }
+   try {
+      const result = await Newsletteremail.find();
+      res.send(result);
+   } catch (error) {
+      res.status(404).json({ message: error.message });
+   }
 }
